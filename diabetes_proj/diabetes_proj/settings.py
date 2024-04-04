69555287805a2aff332fd6643a0c017eae10cdeb
danissimo <danissimo228337@gmail.com>
2024-04-04 21:38:57 +0300

    Added local_settings.py with settings for a database connection for developers

--- diabetes_proj/diabetes_proj/settings.py
+++ diabetes_proj/diabetes_proj/settings.py
@@ -74,16 +74,24 @@ WSGI_APPLICATION = 'diabetes_proj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'diascreenDB',
-        'USER': 'postgres',
-        'PASSWORD': 'danegodiab#020423!',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'diascreenDB',
+#         'USER': 'postgres',
+#         'PASSWORD': 'danegodiab#020423!',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
+
+
+# Local settings for local database connection
+
+try:
+  from utils.local_settings import *
+except ImportError:
+  pass
 
 
 # Password validation
