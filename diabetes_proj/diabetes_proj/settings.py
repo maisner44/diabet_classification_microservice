0baab28b06beed21c855c98fcb9f23c276fcddef
danissimo <danissimo228337@gmail.com>
2024-04-08 17:24:16 +0300

    Start design for main page (markup)

--- diabetes_proj/diabetes_proj/settings.py
+++ diabetes_proj/diabetes_proj/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'MainPage',
+    'login',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'diabetes_proj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['myapp/templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -89,7 +90,7 @@ WSGI_APPLICATION = 'diabetes_proj.wsgi.application'
 # Local settings for local database connection
 
 try:
-  from utils.local_settings import *
+  from utils.local_settings import DATABASES
 except ImportError:
   pass
 
