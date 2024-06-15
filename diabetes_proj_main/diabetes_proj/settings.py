af1d84dc59a6d7744235276f3c51abed034f0c0f
danissimo <danissimo228337@gmail.com>
2024-06-16 00:29:48 +0300

    change setting of database

--- diabetes_proj_main/diabetes_proj/settings.py
+++ diabetes_proj_main/diabetes_proj/settings.py
@@ -97,14 +97,7 @@ WSGI_APPLICATION = 'diabetes_proj.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DATABASE_NAME'),
-        'USER': os.environ.get('DATABASE_USER'),
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-        'HOST': os.environ.get('DATABASE_HOST'),
-        'PORT': os.environ.get('DATABASE_PORT'),
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL')),
 }
 
 
