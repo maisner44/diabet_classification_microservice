3094c01a50708f31e05f3e839ee75a90a1b5afb8
danissimo <danissimo228337@gmail.com>
2024-06-15 21:36:32 +0300

    another fix for deploy

--- diabetes_proj_main/diabetes_proj/settings.py
+++ diabetes_proj_main/diabetes_proj/settings.py
@@ -23,7 +23,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^qrj!a@s!^2q@64m6%iw-um@&3vinvkjq$5v@v#xby(^w%tfil'
+SECRET_KEY = os.getenv('SECRET_KEY', 'your_default_secret_key_here')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
